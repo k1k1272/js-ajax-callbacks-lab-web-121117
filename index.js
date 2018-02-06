828b12409d0fb48db53aadc203dbb1b1b60b4693
github username <github email address>
2018-02-05 23:03:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 $(document).ready(function() {});
 
 function displayError() {
-  $("#errors").innerHTML =
+  document.getElementById("errors").innerHTML =
     "I'm sorry, there's been an error. Please try again.";
+}
+
+function showCommits(ele) {
+  $.get(
+    `https://api.github.com/repos/${ele.dataset.owner}/${
+      ele.dataset.repository
+    }/commits`,
+    data => {
+      $("#details").html(renderCommits(data));
+    }
+  ).fail(displayError);
+}
+
+function renderCommits(data) {
+  let result = data
+    .map(
+      commit => `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+    )
+    .join("");
+  return `<ul>${result}</ul>`;
+}
+
+function renderSearchResults(data) {
+  data.items.map(
+    result => `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${
+      result.owner.login
+    }" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+  );
+}
+
+function searchRepositories() {
+  const searchTerms = $("#searchTerms").val();
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    $("#results").html(renderSearchResults(data));
+  }).fail(error => {
+    displayError();
+  });
 }
